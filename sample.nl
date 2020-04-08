91cbc5ddd58113ce502ddec167a704ed9d40abb0
Nathan Fritzler <nfblaster@live.com>
2020-04-07 21:14:59 -0600

    Move around some stuff (and actually add the licence)

--- sample.nl
+++
@@ -1,9 +0,0 @@
-(define pie 3)             ; defines pie to be 3
- 
-(define (piece str)        ; defines piece as a function
-  (substring str 0 pie))   ;  of one argument
-
-pie
-
-(print
-  (piece "key lime"))
--- sample.rkt
+++ sample.rkt
@@ -1,1 +1,11 @@
-sample.nl
+(define pie 3)             ; defines pie to be 3
+ 
+(define (piece str)        ; defines piece as a function
+  (substring str 0 pie))   ;  of one argument
+
+pie
+
+(print
+  (piece "key lime"))
+;TODO: change this file extension. It's already taken (by another lisp. lol). Perhaps just do rkt.
+
